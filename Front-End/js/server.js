a646aa6d7dbdb20420e527b0cb40c6ecbbab6362
flamebird98 <47006800+flamebird98@users.noreply.github.com>
2020-03-30 09:27:13 -0400

    Front-end running on port 5000

---
+++ Front-End/js/server.js
@@ -0,0 +1,12 @@
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+var path = require("path");
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+ res.sendFile(path.join(__dirname+'/index.html'));
+});
+//Launch listening server on port 8081
+app.listen(5000, function () {
+  console.log('app listening on port 5000!')
+})
